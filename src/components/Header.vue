dc57f71fe38c14df9e3d1f14c4672c2189402335
mindface <in00und@gmail.com>
2021-01-23 20:03:24 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <button @click="onClickBtn">lllll
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Inject,
+  Model,
+  Prop,
+  Provide,
+  Vue,
+  Watch,
+} from "nuxt-property-decorator"
+
+@Component
+export default class Header extends Vue {
+  onClickBtn(){
+    console.log("lll")
+  }
+}
+
+</script>
+
+<style>
+</style>
